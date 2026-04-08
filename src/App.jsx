d029c01004b3fa82faa53d044bb519ab6a3a8807
hellenpaula <helleribeiro29@gmail.com>
2026-04-08 15:26:42 -0300

    feat: aplicando alinhamento e estilo básico no home.

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,12 @@ import Agendamento from './pages/agendamento/Agendamento.jsx'
 import Contato from './pages/contato/Contato.jsx'
 
 
+
 // components:
 import Header from './components/Header/Header.jsx'
+
+import SectionHome from './components/principal/SectionHome/SectionHome.jsx'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -24,6 +28,7 @@ function App() {
     {/* ligar ao header component */}
     <Router>
       <Header />
+      <SectionHome />
       <Routes>
 
         <Route path='/contato' element={<Contato />}/>
---
+++ src/components/principal/SectionHome/SectionHome.jsx
@@ -0,0 +1,28 @@
+
+import './SectionHome.css'
+
+// img:
+import imagem from '../../../assets/img/img home médico paciente.jpg'
+
+function SectionHome() {
+    return(
+        <section className="SectionHome" id="Home">
+
+            <div className="AreaTextoContainerHome">
+                <h2 className="TituloAreaTextoHome">Sua saúde, <br/>
+                Nossa prioridade</h2>
+
+                <p className="ParagrafoAreaTextoHome">
+                    Call out a feature, benefit, or value of your site, then link to a page where people can learn more about it.
+                </p>
+            </div>
+
+            <div className="ImgSectionHome">
+                <img src={imagem} alt="" className='img'/>
+            </div>
+
+        </section>
+    )
+}
+
+export default SectionHome;
