6ec9d36ed9bf927187013bfc40449dcd02dff910
hellenpaula <helleribeiro29@gmail.com>
2026-04-14 22:11:42 -0300

    feat: atualizando slider e setas da seção exames

--- src/components/principal/SectionExames/CardsExames.jsx
+++ src/components/principal/SectionExames/CardsExames.jsx
@@ -1,15 +1,15 @@
 
 // css
 import  './CardsExames.css'
+import { forwardRef } from 'react';
 
-
-function CardsExames(props) {
+const CardsExames = forwardRef((props, ref) => {
     return(
-        <div className="CardExames">
+        <div className="CardExames" ref={ref}>
             <img src={props.ImagemIconeExames} alt="" className="ImgIconeExames" />
             <h4 className="TituloCardExames">{props.TituloCardExames}</h4>
         </div>
     )
-}
+});
 
 export default CardsExames;
--- src/components/principal/SectionExames/SectionExames.jsx
+++ src/components/principal/SectionExames/SectionExames.jsx
@@ -9,6 +9,10 @@ import CardExames from './CardsExames.jsx'
 // imagem
 import ImagemSangue from '../../../assets/icone/icone sangue.png'
 
+import ImagemSeta from '../../../assets/icone/seta.png'
+
+
+import { useRef, useState } from 'react'
 
 function SectionExames() {
 
@@ -19,21 +23,88 @@ function SectionExames() {
             TituloCardExames: 'Coleta de sangue'
         },
         {
-            Id: 1,
+            Id: 2,
             ImagemIconeExames: ImagemSangue,
-            TituloCardExames: 'Coleta de sangue'
+            TituloCardExames: 'Raio-X'
         },
         {
-            Id: 1,
+            Id: 3,
             ImagemIconeExames: ImagemSangue,
-            TituloCardExames: 'Coleta de sangue'
+            TituloCardExames: 'Eletrocardiograma'
         },
         {
-            Id: 1,
+            Id: 4,
             ImagemIconeExames: ImagemSangue,
-            TituloCardExames: 'Coleta de sangue'
-        }
+            TituloCardExames: 'Ultrassom'
+        },
+        {
+            Id: 5,
+            ImagemIconeExames: ImagemSangue,
+            TituloCardExames: 'Ressonância'
+        },
+        {
+            Id: 6,
+            ImagemIconeExames: ImagemSangue,
+            TituloCardExames: 'Holter e MAPA'
+        },
+        {
+            Id: 7,
+            ImagemIconeExames: ImagemSangue,
+            TituloCardExames: 'Endoscopia'
+        },
+        {
+            Id: 8,
+            ImagemIconeExames: ImagemSangue,
+            TituloCardExames: 'Tomografia'
+        },
+       
     ]
+
+
+     const carrossel = useRef(null);
+     const cardRef = useRef(null);
+
+
+     function ArrowLeftClick(e) {
+        e.preventDefault();
+
+        const larguraCard = cardRef.current.offsetWidth;
+        
+        const estilosContainer = window.getComputedStyle(carrossel.current);
+
+        const gap = parseInt(estilosContainer.gap);
+
+        const larguraScroll = larguraCard + gap;
+        // const novoIndex = Math.max(currentIndex - 1, 0);
+        // setCurrentIndex(novoIndex);
+        carrossel.current.scrollLeft -= larguraScroll;
+
+
+        // setTimeout(() => {
+        //     e.currentTarget.style.opacity = "0.5";
+        // }, 200);
+        
+    }
+
+    const ArrowRightClick = (e) => {
+        e.preventDefault();
+
+        const larguraCard = cardRef.current.offsetWidth;
+        const estilosContainer = window.getComputedStyle(carrossel.current);
+
+        const gap =  parseInt(estilosContainer.gap);
+
+        const larguraScroll =
+        larguraCard + gap;
+
+        // const novoIndex = Math.min(currentIndex + 1, total - 1);
+        // setCurrentIndex(novoIndex);
+        carrossel.current.scrollLeft += larguraScroll;
+
+        // setTimeout(() => {
+        //     e.currentTarget.style.opacity = '0.5';
+        // }, 200);
+    }
 
     return(
         <section className="SectionExames" id='Exames'>
@@ -41,15 +112,23 @@ function SectionExames() {
                 Exames oferecidos
             </h3>
 
-            <div className="ContainerCardsAreaExames">
-                {DadosExames.map((dado) => (
+            <div className="ContainerCardsAreaExames" ref={carrossel} >
+                {DadosExames.map((dado, index) => (
                     <CardExames
                     Id= {dado.Id}
+                    ref= {index === 0 ? cardRef : null}
                     ImagemIconeExames = {dado.ImagemIconeExames}
                     TituloCardExames = {dado.TituloCardExames}
                     />
                 ))}
             </div>
+
+            <div className="ContainerSetasCarrossel">
+                <button onClick={ArrowLeftClick}><img src={ImagemSeta} alt="" className="SetaEsquerdaCarrossel" /></button>
+
+                <button onClick={ArrowRightClick}><img src={ImagemSeta} alt="" className="SetaDireitaCarrossel" /></button>
+            </div>
+
 
             <div className="ContainerBotaoAreaExames">
                 <button className='BotaoExames'>
