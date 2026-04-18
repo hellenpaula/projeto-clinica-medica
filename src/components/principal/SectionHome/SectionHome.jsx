8e23ac7eb3ac977828bc4e63685a8ee1b65df11c
hellenpaula <helleribeiro29@gmail.com>
2026-04-17 23:34:03 -0300

    add: adicionando novas atualizações no style do home

--- src/components/principal/SectionHome/SectionHome.jsx
+++ src/components/principal/SectionHome/SectionHome.jsx
@@ -4,23 +4,27 @@ import './SectionHome.css'
 // img:
 import imagem from '../../../assets/img/img home médico paciente.jpg'
 
+import imagemMedicos from '../../../assets/img/img area home.jpg'
+
+import imagemMedicosAzul from '../../../assets/img/img area home azul.jpg'
+
+
 function SectionHome() {
     return(
-        <section className="SectionHome" id="Home">
-
-            <div className="AreaTextoContainerHome">
-                <h2 className="TituloAreaTextoHome">Sua saúde, <br/>
-                Nossa prioridade</h2>
-
-                <p className="ParagrafoAreaTextoHome">
-                    Call out a feature, benefit, or value of your site, then link to a page where people can learn more about it.
-                </p>
+        <section className="ContainerSectionHome">
+            
+            <div className="SectionHome" id="Home">
+                <div className="AreaTextoContainerHome">
+                    <h2 className="TituloAreaTextoHome">Sua saúde, <br/>
+                    Nossa prioridade</h2>
+                    <p className="ParagrafoAreaTextoHome">
+                        Call out a feature, benefit, or value of your site, then link to a page where people can learn more about it.
+                    </p>
+                </div>
+                {/* <div className="ImgSectionHome">
+                    <img src={imagem} alt="" className='img'/>
+                </div> */}
             </div>
-
-            {/* <div className="ImgSectionHome">
-                <img src={imagem} alt="" className='img'/>
-            </div> */}
-
         </section>
     )
 }
